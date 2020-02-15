2f01c22433352ff71a53a25bb5482a82e8c2ef1c
Łukasz Klich <klich.lukasz@gmail.com>
2020-02-15 07:18:57 +0100

    Add season 2020 information - not used yet

--- src/app/season.service.ts
+++ src/app/season.service.ts
@@ -3,6 +3,7 @@ import { Race } from './race';
 import { Season } from './season';
 import { Observable, of } from 'rxjs';
 import s2019 from '../assets/seasons/2019.json';
+import s2020 from '../assets/seasons/2020.json';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,10 @@ export class SeasonService {
 
   constructor() { }
 
+  allRaces = s2019.races.concat(s2020.races);
 
   getSeason(year: number): Observable<Season> {
+
     return s2019;
   }
 
@@ -21,6 +24,6 @@ export class SeasonService {
   }
 
   getRace(id: number): Observable<Race> {
-    return of(s2019.races.find(race => race.id === id));
+    return of(this.allRaces.find(race => race.id === id));
   }
 }
